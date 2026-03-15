0a1c8c83b33f256a2ee2d61e53a3ee862de3acd9
3DGlit <afbgraphics45230@gmail.com>
2026-03-15 15:23:16 +0100

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -1,41 +1,56 @@
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
 75,
-window.innerWidth / 600,
+window.innerWidth / window.innerHeight,
 0.1,
 1000
-)
+);
 
-const renderer = new THREE.WebGLRenderer({antialias:true})
-renderer.setSize(window.innerWidth,600)
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-document.getElementById("viewer").appendChild(renderer.domElement)
+document.getElementById("viewer").appendChild(renderer.domElement);
 
-const controls = new THREE.OrbitControls(camera,renderer.domElement)
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-camera.position.z = 5
+camera.position.set(0, 0, 3);
 
-const light = new THREE.HemisphereLight(0xffffff,0x444444,2)
-scene.add(light)
+const light = new THREE.HemisphereLight(0xffffff, 0x444444, 2);
+scene.add(light);
 
-const loader = new THREE.GLTFLoader()
+const loader = new THREE.GLTFLoader();
 
-loader.load("models/yes.glb",function(gltf){
+loader.load(
+"models/yes.glb",
 
-const model = gltf.scene
-scene.add(model)
+function (gltf) {
 
-})
+const model = gltf.scene;
+scene.add(model);
 
-function animate(){
+console.log("MODELE CHARGE");
 
-requestAnimationFrame(animate)
+},
 
-controls.update()
+undefined,
 
-renderer.render(scene,camera)
+function (error) {
+
+console.error("ERREUR GLB :", error);
 
 }
 
-animate()
+);
+
+function animate() {
+
+requestAnimationFrame(animate);
+
+controls.update();
+
+renderer.render(scene, camera);
+
+}
+
+animate();
