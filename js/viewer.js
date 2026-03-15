cb01fec828952557f6a2c94aa70ee0da7be3015b
3DGlit <afbgraphics45230@gmail.com>
2026-03-15 15:05:17 +0100

    Update viewer.js

--- js/viewer.js
+++ js/viewer.js
@@ -4,7 +4,7 @@ const camera = new THREE.PerspectiveCamera(
 75,
 window.innerWidth/600,
 0.1,
-10000
+100000
 )
 
 const renderer = new THREE.WebGLRenderer({antialias:true})
@@ -12,48 +12,41 @@ renderer.setSize(window.innerWidth,600)
 
 document.getElementById("viewer").appendChild(renderer.domElement)
 
-const controls = new THREE.OrbitControls(camera,renderer.domElement)
+const controls = new THREE.OrbitControls(camera, renderer.domElement)
 
-camera.position.set(0,0,200)
+scene.add(new THREE.AmbientLight(0xffffff,1))
 
 const light = new THREE.DirectionalLight(0xffffff,1)
-light.position.set(100,100,100)
-
+light.position.set(200,200,200)
 scene.add(light)
-scene.add(new THREE.AmbientLight(0xffffff,0.7))
-
-let model
 
 const loader = new THREE.GLTFLoader()
 
 loader.load("models/yes.glb", function(gltf){
 
-model = gltf.scene
-
+const model = gltf.scene
 scene.add(model)
 
+/* calculer la taille du modèle */
+
 const box = new THREE.Box3().setFromObject(model)
+const size = box.getSize(new THREE.Vector3()).length()
 const center = box.getCenter(new THREE.Vector3())
 
-model.position.sub(center)
+/* centrer modèle */
+
+model.position.x -= center.x
+model.position.y -= center.y
+model.position.z -= center.z
+
+/* placer caméra automatiquement */
+
+camera.position.set(0, size/2, size*1.5)
+
+controls.target.set(0,0,0)
+controls.update()
 
 })
-
-function changeColor(color){
-
-model.traverse(function(child){
-
-if(child.isMesh){
-
-child.material.color.set(color)
-
-}
-
-})
-
-}
-
-window.changeColor = changeColor
 
 function animate(){
 
